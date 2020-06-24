fd51bee657feeb34c5d1d4d71d2c8d8260118fec
unknown <joelsunny.ch@gmail.com>
2020-06-24 21:21:27 +0530

    added collision

--- flappy-bird/game.py
+++ flappy-bird/game.py
@@ -1,6 +1,10 @@
 import pygame
 import pygame.freetype
+import random
 from pprint import pprint
+from collections import namedtuple
+
+Point = namedtuple('Point', 'x y')
 
 class Grid:
     WIDTH = 720
@@ -31,11 +35,15 @@ class Pillar:
         self.gap = 150
         self.t = 0
 
+    def reset_height(self):
+        self.h = random.randint(0,3)*100 + 100
+
     def move(self):
         self.t = clock.get_rawtime()/100
         self.x += self.v_x*self.t
         if self.x < -self.w:
             self.x = 720
+            self.reset_height()
 
     def draw(self, surface):
         color = (0,255,0)
@@ -56,7 +64,36 @@ class Bird:
     def jump(self):
         self.g_y = -30
     
+    def check_collision(self):
+        global pillars
+        def doOverlap(l1, r1, l2, r2, top=True): 
+            
+            # If one rectangle is on left side of other 
+            if(l1.x >= r2.x or r1.x <= l2.x): 
+                return False
+        
+            # If one rectangle is above other 
+            if(l1.y >= r2.y and top): 
+                return False
+            
+            if(r1.y <= l2.y and not top): 
+                return False
+            
+            return True
+  
+        for pillar in pillars:
+            if doOverlap(Point(self.x, self.y), Point(self.x+20, self.y+20), Point(pillar.x,pillar.y), Point(pillar.x+pillar.w,pillar.y+pillar.h)):
+                return True
+            elif doOverlap(Point(self.x, self.y), Point(self.x+20, self.y+20), Point(pillar.x, pillar.y + pillar.h + pillar.gap), Point(pillar.x+pillar.w,720), top=False):
+                return True
+
+        return False
+
     def move(self):
+        if self.check_collision():
+            pygame.quit()
+            main()
+
         self.t = clock.get_rawtime()/100
         print(f"t = {self.t}")
         # self.x += self.v_x*self.t
@@ -74,7 +111,7 @@ class Bird:
             self.v_y = 0
         if self.x > 700:
             self.x = 700
-
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -89,21 +126,28 @@ class Bird:
                 elif keys[pygame.K_r]:
                     pygame.quit()
                     main()
+
 def main():
-    global g, clock
+    global g, clock, pillars
     done = False
 
     clock = pygame.time.Clock()
     g = Grid()
     b = Bird()
-    p = Pillar(720, 200)
+    # create 3 pillars
+    p1 = Pillar(720, 200)
+    p2 = Pillar(1000, 300)
+    p3 = Pillar(1280, 100)
+    pillars = [p1,p2,p3]
 
     while not done:
         pygame.time.delay(50)
         clock.tick(10)
         b.move()
         g.draw(b)
-        p.draw(g.surface)
+        p1.draw(g.surface)
+        p2.draw(g.surface)
+        p3.draw(g.surface)
         pygame.display.update()
 
 main()
